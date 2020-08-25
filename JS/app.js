dfd0fdbb64c37d9e6f1a3b5d5f8ab32cfd917130
steveo1978 <steve.r.marical@outlook.com>
2020-08-24 20:14:50 -0700

    completed JS for lab11

--- JS/app.js
+++ JS/app.js
@@ -4,91 +4,103 @@
 var imgElOne = document.getElementById('image-one');
 var imgElTwo = document.getElementById('image-two');
 var imgElThree = document.getElementById('image-three');
-
+var clicksAllowed = 25;
+var numberOfClicks = 0;
+var renderArray = [];
 var imgArray = [];
 
-function Picture(image, src) {
+function Picture(name, src) {
   this.viewed = 0;
   this.clicked = 0;
   this.src = src;
-  this.image = image;
+  this.name = name;
 
   imgArray.push(this);
 }
 
-new Picture('image-one', './imgs/bag.jpg');
-new Picture('image-two', './imgs/banana.jpg');
-new Picture('image-three', './imgs/bathroom.jpg');
-new Picture('image-four', './imgs/boots.jpg');
-new Picture('image-five', './imgs/breakfast.jpg');
-new Picture('image-six', './imgs/bubblegum.jpg');
-new Picture('image-seven', './imgs/chair.jpg');
-new Picture('image-eight', './imgs/cthulhu.jpg');
-new Picture('image-nine', './imgs/dog-duck.jpg');
-new Picture('image-ten', './imgs/dragon.jpg');
-new Picture('image-eleven', './imgs/pen.jpg');
-new Picture('image-twelve', './imgs/pet-sweep.jpg');
-new Picture('image-thirteen', './imgs/scissors.jpg');
-new Picture('image-fourteen', './imgs/shark.jpg');
-new Picture('image-fifteen', './imgs/sweep.png');
-new Picture('image-sixteen', './imgs/tauntaun.jpg');
-new Picture('image-seventeen', './imgs/unicorn.jpg');
-new Picture('image-eighteen', './imgs/usb.gif');
-new Picture('image-nineteen', './imgs/water-can.jpg');
-new Picture('image-twenty', './imgs/wine-glass.jpg');
+new Picture('bag', './imgs/bag.jpg');
+new Picture('banana', './imgs/banana.jpg');
+new Picture('bathroom', './imgs/bathroom.jpg');
+new Picture('boots', './imgs/boots.jpg');
+new Picture('breakfast', './imgs/breakfast.jpg');
+new Picture('bubblegum', './imgs/bubblegum.jpg');
+new Picture('chair', './imgs/chair.jpg');
+new Picture('cthulhu', './imgs/cthulhu.jpg');
+new Picture('dog-duck', './imgs/dog-duck.jpg');
+new Picture('dragon', './imgs/dragon.jpg');
+new Picture('pen', './imgs/pen.jpg');
+new Picture('pet-sweep', './imgs/pet-sweep.jpg');
+new Picture('scissors', './imgs/scissors.jpg');
+new Picture('shark', './imgs/shark.jpg');
+new Picture('sweep', './imgs/sweep.png');
+new Picture('tauntaun', './imgs/tauntaun.jpg');
+new Picture('unicorn', './imgs/unicorn.jpg');
+new Picture('usb', './imgs/usb.gif');
+new Picture('water-can', './imgs/water-can.jpg');
+new Picture('wine-glass', './imgs/wine-glass.jpg');
 
-function renderImages() {
-  var imgElOne = imgArray[(randomNumber(imgArray.length))];
-  var imgElTwo = imgArray[(randomNumber(imgArray.length))];
-  var imgElThree = imgArray[(randomNumber(imgArray.length))];
 
-  imgElOne.src = imgArray[0].src;
-  imgElTwo.src = imgArray[1].src;
-  imgElThree.src = imgArray[2].src;
-
-  function randomNumber(max) {
-    return Math.floor(Math.random() * max.length);
-  }
-}
 
 renderImages();
-
-var imgOne = imgArray[randomNumber(imgArray.length)];
-var imgTwo = imgArray[randomNumber(imgArray.length)];
-var imgThree = imgArray[randomNumber(imgArray.length)];
-
-while(imgOne === imgTwo) {
-  imgTwo === imgArray[randomNumber(imgArray.length)];
-  imgThree === imgArray[randomNumber(imgArray.length)];
-}
-
-imgElOne.alt = imgOne.src;
-imgElOne.src = imgOne.image;
-
-imgElTwo.alt = imgTwo.src;
-imgElTwo.src = imgTwo.image;
-
-imgElThree.alt = imgThree.src;
-imgElThree.src = imgThree.image;
-
-imgOne.viewed++;
-imgTwo.viewed++;
-imgThree.viewed++;
-
 function randomNumber(max) {
   return Math.floor(Math.random() * max);
+}
+
+function createRenderArray() {
+  while (renderArray.length > 0) {
+    renderArray.pop();
+  }
+  while (renderArray.length < 3) {
+    var i = randomNumber(imgArray.length);
+    while (renderArray.includes(i)) {
+      i = randomNumber(imgArray.length);
+    }
+    renderArray.push(i);
+  }
+  // console.log(renderArray);
+}
+
+function renderImages() {
+  createRenderArray();
+  var imgOne = imgArray[renderArray[0]];
+  var imgTwo = imgArray[renderArray[1]];
+  var imgThree = imgArray[renderArray[2]];
+
+  imgElOne.alt = imgOne.name;
+  imgElOne.src = imgOne.src;
+
+  imgElTwo.alt = imgTwo.name;
+  imgElTwo.src = imgTwo.src;
+
+  imgElThree.alt = imgThree.name;
+  imgElThree.src = imgThree.src;
+
+  imgOne.viewed++;
+  imgTwo.viewed++;
+  imgThree.viewed++;
+}
+
+function eventHandler(e) {
+  console.log(e.target.alt);
+  numberOfClicks++;
+
+
+  for (var i = 0; i < imgArray.length; i++) {
+    if (imgArray[i].name === e.target.alt) {
+      imgArray[i].clicked++;
+    }
+  }
+  renderImages();
+
+  if (numberOfClicks === clicksAllowed) {
+    imgElOne.removeEventListener('click', eventHandler);
+    imgElTwo.removeEventListener('click', eventHandler);
+    imgElThree.removeEventListener('click', eventHandler);
+
+  }
 }
 
 imgElOne.addEventListener('click', eventHandler);
 imgElTwo.addEventListener('click', eventHandler);
 imgElThree.addEventListener('click', eventHandler);
-
-function eventHandler(e) {
-  for(var i = 0; i < imgArray.length; i++) {
-    console.log(e.target.alt);
-    if(imgArray[i].image === e.target.alt) {
-      imgArray[i].clicked++;
-    }
-  }
-}
 
